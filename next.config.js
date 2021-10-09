6e614847578f55fb85bdf39b4d529582569332c8
Omid Badkoubeh <omid.badkoubeh@gmail.com>
2021-10-10 00:17:29 +0330

    fix: image urls added

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  images: {
+    domains: [
+      'res.cloudinary.com',
+      'avatars.githubusercontent.com',
+      'imgur.com',
+    ],
+  },
+};
